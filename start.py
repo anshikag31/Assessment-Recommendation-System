8650d3f669874653852e44302b41df3aa8251cce
anshikag31 <105929985+anshikag31@users.noreply.github.com>
2025-05-10 03:16:01 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,5 +2,7 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    print("🔁 Starting server...")
+    port = int(os.environ.get("PORT", 10000))
+    print(f"🌐 Launching on port {port}")
     uvicorn.run("main:app", host="0.0.0.0", port=port)
