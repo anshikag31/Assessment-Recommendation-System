81ed0fea250e9e6350bfafd74ffed632ac940230
anshikag31 <105929985+anshikag31@users.noreply.github.com>
2025-05-10 03:37:37 +0530

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-import os
-import uvicorn
-
-if __name__ == "__main__":
-    print("🔁 Starting server...")
-    port = int(os.environ.get("PORT", 10000))
-    print(f"🌐 Launching on port {port}")
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
